089a762fd189df40f2387a5e48188c48d5c78cee
Slipsoon <sebastian.slipek@hotmail.com>
2020-04-01 22:14:36 +0200

    Menu - make animate menu

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const navBar = document.querySelector('nav.mobile');
+    const logo = document.querySelector('nav.mobile ul li');
     const menuButton = document.querySelector('nav.mobile ul li:nth-child(2)');
     const mobileMainPage = document.querySelector('.mobile-wrapper');
     const mobileMenu = document.querySelector('.mobile-menu');
     const returnButton = document.querySelector('.return img');
 
-    menuButton.addEventListener('click', () => {
+    function animateMenu() {
+        menuButton.style.display = 'none';
+        logo.style.display = 'none';
+        navBar.classList.toggle('animation');
+        setTimeout(showMenu, 500);
+    }
+
+
+    function showMenu() {
         mobileMainPage.style.display = 'none';
         mobileMenu.style.display = 'block';
-    })
+        navBar.classList.toggle('animation');
+        menuButton.style.display = 'block';
+        logo.style.display = 'block';
+    }
 
-    returnButton.addEventListener('click', () => {
+    function hideMenu() {
         mobileMainPage.style.display = 'block';
         mobileMenu.style.display = 'none';
-    })
+    }
+
+    returnButton.addEventListener('click', hideMenu);
+    menuButton.addEventListener('click', animateMenu);
 })

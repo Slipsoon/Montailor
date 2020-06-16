6b06111a73ba3b6dcadc065d5816af204ca83e4b
Slipsoon <sebastian.slipek@hotmail.com>
2020-06-16 23:35:42 +0200

    Add current/total counter image feature, add imageview for desktop support

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -5,13 +5,25 @@ const mobileWrapper = document.querySelector('div.mobile-wrapper');
 const exitBtn = document.querySelector('div.area');
 const prevBtn = document.querySelector('button.previous');
 const nextBtn = document.querySelector('button.next');
+const currentImageCounter = document.querySelector('div.current-image');
+const allImagesCounter = document.querySelector('div.all-images');
 
 // -- GALLERY IMAGE FEATURES -- 
 
 // Contains current opened image index
 let currentImageIndex;
 
+// Images Counter
+
+// Current image counter
+function changeCurrentImgCounter() {
+    currentImageCounter.textContent = currentImageIndex + 1;
+}
+
+// Total images counter 
+
 // Get list (array) of images
+
 const imageArray = function getImageList() {
     let imagesList = [];
 
@@ -19,10 +31,12 @@ const imageArray = function getImageList() {
         imagesList.push(image);
     })
 
+    allImagesCounter.textContent = imagesList.length;
     return imagesList;
 }
 
 // Get image index
+
 function getImageIndex(currentImage) {
 
     let currentImageIndex;
@@ -37,6 +51,7 @@ function getImageIndex(currentImage) {
 }
 
 // Get next image
+
 function getNextImage() {
 
     if (currentImageIndex == imageArray().length - 1) {
@@ -47,9 +62,11 @@ function getNextImage() {
 
     const nextImgSrc = imageArray()[currentImageIndex].getAttribute('src');
     imageResize.querySelector('img').setAttribute('src', nextImgSrc);
+    changeCurrentImgCounter();
 }
 
 // Get previous image
+
 function getPreviousImage() {
 
     if (currentImageIndex == 0) {
@@ -60,11 +77,25 @@ function getPreviousImage() {
 
     const nextImgSrc = imageArray()[currentImageIndex].getAttribute('src');
     imageResize.querySelector('img').setAttribute('src', nextImgSrc);
+    changeCurrentImgCounter();
 }
 
 // Listeners for next and previous buttons
+
 prevBtn.addEventListener('click', () => getPreviousImage());
 nextBtn.addEventListener('click', () => getNextImage());
+
+document.addEventListener('keydown', function (e) {
+
+    if (imageResize.style.display == 'block' && e.key == 'ArrowLeft')
+        getPreviousImage();
+});
+
+document.addEventListener('keydown', function (e) {
+
+    if (imageResize.style.display == 'block' && e.key == 'ArrowRight')
+        getNextImage();
+});
 
 // Get the called img source
 
@@ -73,6 +104,7 @@ images.forEach(image => {
 
         let currentImage = image;
         currentImageIndex = getImageIndex(currentImage);
+        currentImageCounter.textContent = parseInt(currentImageIndex, 10) + 1;
 
         imgSrc = image.getAttribute('src');
         resizeImage(imgSrc);
@@ -89,11 +121,21 @@ function resizeImage(setImgSrc) {
     imageResize.appendChild(createImgElement);
 }
 
+// Imageview display exit listeners
+
+exitBtn.addEventListener('click', () => imageViewExit());
+
+document.addEventListener('keydown', function (e) {
+
+    if (imageResize.style.display == 'block' && e.key == 'Escape')
+        imageViewExit();
+})
+
 // Event for returning to gallery web page
 
-exitBtn.addEventListener('click', () => {
+function imageViewExit() {
     navigationBar.style.display = 'block';
     imageResize.style.display = 'none';
     const getImage = imageResize.querySelector('img');
     imageResize.removeChild(getImage);
-})
+}

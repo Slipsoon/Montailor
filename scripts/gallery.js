3f569d1654070260d95bb7b362bac2d1337939b6
Slipsoon <sebastian.slipek@hotmail.com>
2020-06-16 18:49:12 +0200

    Add feature for mobile gallery change images

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,12 +3,78 @@ const navigationBar = document.querySelector('nav.mobile');
 const imageResize = document.querySelector('div.image-resize');
 const mobileWrapper = document.querySelector('div.mobile-wrapper');
 const exitBtn = document.querySelector('div.area');
+const prevBtn = document.querySelector('button.previous');
+const nextBtn = document.querySelector('button.next');
+
+// -- GALLERY IMAGE FEATURES -- 
+
+// Contains current opened image index
+let currentImageIndex;
+
+// Get list (array) of images
+const imageArray = function getImageList() {
+    let imagesList = [];
+
+    images.forEach(image => {
+        imagesList.push(image);
+    })
+
+    return imagesList;
+}
+
+// Get image index
+function getImageIndex(currentImage) {
+
+    let currentImageIndex;
+
+    for (index in imageArray()) {
+
+        if (currentImage == imageArray()[index])
+            currentImageIndex = index;
+    }
+
+    return currentImageIndex;
+}
+
+// Get next image
+function getNextImage() {
+
+    if (currentImageIndex == imageArray().length - 1) {
+        currentImageIndex = 0;
+    } else {
+        currentImageIndex++;
+    }
+
+    const nextImgSrc = imageArray()[currentImageIndex].getAttribute('src');
+    imageResize.querySelector('img').setAttribute('src', nextImgSrc);
+}
+
+// Get previous image
+function getPreviousImage() {
+
+    if (currentImageIndex == 0) {
+        currentImageIndex = imageArray().length - 1;
+    } else {
+        currentImageIndex--;
+    }
+
+    const nextImgSrc = imageArray()[currentImageIndex].getAttribute('src');
+    imageResize.querySelector('img').setAttribute('src', nextImgSrc);
+}
+
+// Listeners for next and previous buttons
+prevBtn.addEventListener('click', () => getPreviousImage());
+nextBtn.addEventListener('click', () => getNextImage());
 
 // Get the called img source
 
 images.forEach(image => {
     image.addEventListener("click", () => {
-        const imgSrc = image.getAttribute('src');
+
+        let currentImage = image;
+        currentImageIndex = getImageIndex(currentImage);
+
+        imgSrc = image.getAttribute('src');
         resizeImage(imgSrc);
     });
 })
@@ -23,7 +89,7 @@ function resizeImage(setImgSrc) {
     imageResize.appendChild(createImgElement);
 }
 
-// Event for returning to web page
+// Event for returning to gallery web page
 
 exitBtn.addEventListener('click', () => {
     navigationBar.style.display = 'block';
